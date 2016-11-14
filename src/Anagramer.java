a5729c716659d74cc21994c67d699c31a93b7113
Anton Kalmykov <kalmykov.anton@gmail.com>
2016-11-14 18:04:35 +0200

    Total rework: readability increase
    Rewrite input without use of Scanner
    Rewrite reverse()
    Rename reverse() to makeAnagramOfEachWord()
    Add makeAnagramOfWord()
    Use StringBuilder instead of concatenation
    Delete is LatinLetter(), use Character.isLetter() instead
    Correct formatting

--- src/Anagramer.java
+++ src/Anagramer.java
@@ -1,50 +1,65 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class Anagramer {
-	public static void main(String [] args) {
-		Scanner scan = new Scanner (System.in);
-		
-		System.out.print("Enter phrase: ");
-		String phrase = scan.nextLine();
-		
-		String[] words = phrase.split(" ");
-		
-		for (String word : words) {
-			System.out.print(reverse(word).concat(" "));
+	public static void main(String[] args) {
+		try (InputStream inputStream = System.in;
+				Reader inputStreamReader = new InputStreamReader(inputStream);
+				BufferedReader reader = new BufferedReader(inputStreamReader)) {
+
+			System.out.print("Enter text: ");
+			String text = reader.readLine();
+			String result = makeAnagramOfEachWord(text);
+			System.out.println(result);
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
-	private static String reverse(String word){
-		final int WORD_LENGTH = word.length();
-		
+
+	private static String makeAnagramOfEachWord(String text) {
+		String delimiter = " ";
+		String separator = " ";
+		String[] words = text.split(delimiter);
+		int numberOfWords = words.length;
+		int lastWordIndex = numberOfWords - 1;
+
+		StringBuilder builder = new StringBuilder();
+
+		for (int wordIndex = 0; wordIndex < numberOfWords; wordIndex++) {
+			String anagram = makeAnagramOfWord(words[wordIndex]);
+			builder.append(anagram);
+
+			if (wordIndex != lastWordIndex) {
+				builder.append(separator);
+			}
+		}
+
+		return builder.toString();
+	}
+
+	private static String makeAnagramOfWord(String word) {
+		int wordLength = word.length();
 		char[] chars = word.toCharArray();
-		char[] result = new char[WORD_LENGTH];
-		
-		for (int i = 0; i < WORD_LENGTH; i++) {
-			if (!isLatinLetter(chars[i])){
-				result[i] = chars[i];
+		StringBuilder builder = new StringBuilder(wordLength);
+
+		for (char ch : chars) {
+			if (Character.isLetter(ch)) {
+				builder.append(ch);
 			}
 		}
-		
-		for (int i = 0; i < WORD_LENGTH; i++){
-			if (isLatinLetter(chars[i])){
-				
-				for (int j = WORD_LENGTH - 1; j >= 0; j--){
-					if (result[j] == 0) {
-						result[j] = chars[i];
-						break;
-					}
-				}
+
+		builder.reverse();
+
+		for (int charIndex = 0; charIndex < wordLength; charIndex++) {
+			if (!Character.isLetter(chars[charIndex])) {
+				builder.insert(charIndex, chars[charIndex]);
 			}
 		}
-		return new String(result);
-	}
-	
-	private static boolean isLatinLetter(char ch){
-		if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
-			return true;
-		}
-		return false;
-		
+
+		return builder.toString();
 	}
 }
