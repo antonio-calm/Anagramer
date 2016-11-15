9a4a94631024719ea91a009f7523adc66d5baf98
Anton Kalmykov <kalmykov.anton@gmail.com>
2016-11-15 12:24:51 +0200

    Delete InputStream

--- src/com/kalmykov/foxminded/anagramer/Anagramer.java
+++ src/com/kalmykov/foxminded/anagramer/Anagramer.java
@@ -1,14 +1,12 @@
 package com.kalmykov.foxminded.anagramer;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class Anagramer {
 	public static void main(String[] args) throws IOException {
-		InputStream inputStream = System.in;
-		Reader inputStreamReader = new InputStreamReader(inputStream);
+		Reader inputStreamReader = new InputStreamReader(System.in);
 		BufferedReader reader = new BufferedReader(inputStreamReader);
 
 			System.out.print("Enter text: ");
